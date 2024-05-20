90874c19f8c57328f0cf87c4c8e248b25b551271
m-takeshima <m.takeshima.cingroup2012@gmail.com>
2024-05-20 16:31:50 +0900

    インデント追加

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -2,12 +2,14 @@ package basicSyntax;
 
 public class Variables {
 	public static void main(String[] args) {
-	int num1 = 80;
-	double num2 = 0.3;
-	System.out.println(num1);
-	System.out.println(num2);
-	System.out.println("私の好きな整数は" + num1 + "です");
-	System.out.println("私の好きな少数は" + num2 + "です");
+		int num1 = 80;
+		double num2 = 0.3;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		System.out.println("私の好きな整数は" + num1 + "です");
+		System.out.println("私の好きな少数は" + num2 + "です");
 	
 	}
 }
