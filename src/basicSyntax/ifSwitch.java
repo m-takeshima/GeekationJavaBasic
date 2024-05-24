2a4636785091b2639a772826b765ab4faa0bc1e6
m-takeshima <m.takeshima.cingroup2012@gmail.com>
2024-05-24 20:18:38 +0900

    if,if elseの後ろに半角スペース追加

--- src/basicSyntax/ifSwitch.java
+++ src/basicSyntax/ifSwitch.java
@@ -7,9 +7,9 @@ public class ifSwitch {
 		
 		int num = 3;
 		
-		if(num % 2 == 0) {
+		if (num % 2 == 0) {
 			System.out.println("2の倍数です。");
-		} else if(num % 3 == 0) {
+		} else if (num % 3 == 0) {
 			System.out.println("3の倍数です。");
 		} else {
 			System.out.println("どちらでもありません");
@@ -26,7 +26,7 @@ public class ifSwitch {
 			case "東京":
 				System.out.println("東京都");
 				break;
-			
+				
 			case "京都":
 				System.out.println("京都府");
 				break;
