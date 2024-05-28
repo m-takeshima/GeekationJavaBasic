b10c88f0f10efcd8059f0c1ddd0d530892ef4c38
m-takeshima <m.takeshima.cingroup2012@gmail.com>
2024-05-28 13:06:38 +0900

    forやifと()の間に半角スペースの追加とインデントの修正

--- src/basicSyntax/ForWhile.java
+++ src/basicSyntax/ForWhile.java
@@ -6,14 +6,14 @@ public class ForWhile {
 		//実装内容１		
 		int num = 1;
 		
-		while(num <= 100) {
-			if(num % 3 == 0) {
+		while (num <= 100) {
+			if (num % 3 == 0) {
 				System.out.println("Fizz");
 			}
-			if(num % 5 ==0 ) {
+			if (num % 5 == 0) {
 				System.out.println("Buzz");
 			}
-			if(num % 5 ==0||num % 3 == 0 ) {
+			if (num % 5 == 0 || num % 3 == 0) {
 				System.out.println("FizzBuzz");
 			}
 			
@@ -23,19 +23,18 @@ public class ForWhile {
 		//実装内容２
 		String[] words = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
 		
-		for(String word:words) {
-			if(word == "Java") {
+		for (String word:words) {
+			if (word == "Java") {
 				System.out.println("現在学習中の言語はJavaです。");
 				
 				continue;
-		}
-			if(word == "HTML") {
+			}
+			if (word == "HTML") {
 				System.out.println("HTMLはプログラミング言語ではありません。");
 				
 				break;
-		}
+			}
 			System.out.println(word);
 		}
 	}
-
 }
